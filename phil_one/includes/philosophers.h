42e464886818f417ca4a42f46f77cdc16fd26160
mint <officialscientist42@gmail.com>
2020-06-13 16:51:17 -0700

    add,fix(philosophers.h): rename macro and rename struct

--- phil_one/includes/philosophers.h
+++ phil_one/includes/philosophers.h
@@ -1,10 +1,14 @@
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
-# define NUM_NEEDED_ARGS 4
+# define NUM_ARGS_NEEDED 4
+# define NUM_ARGS_ALLOWED 5
 
-struct s_monastery;
+# define TRUE 1
+# define FALSE 0
 
-int		run_simulation(struct s_monastery *mon);
+struct s_state;
+
+int		run_simulation(struct s_state *state);
 
 #endif
