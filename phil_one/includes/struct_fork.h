4553eba6f286744a9871f4482873d3900ee03918
mint <officialscientist42@gmail.com>
2020-06-21 12:26:35 -0700

    adding fork functionality

--- phil_one/includes/struct_fork.h
+++ phil_one/includes/struct_fork.h
@@ -7,7 +7,6 @@
 **	create	-> wrapper constructor (allocates instance. calls setup)
 **	destroy	-> wrapper destructor (frees instance. calls cleanup)
 **
-**
 **	always use unsigned types when dealing with something that should be unsigned
 **	TRUE 1, FALSE 0, SUCCESS 1, ERROR 0
 **	what happens if in c++ string = string2 assignment fails, or other construction failures?
@@ -17,14 +16,16 @@
 **	in_use			true or false
 */
 
-struct				s_fork
+struct		s_fork
 {
-	char			in_use;
+	char	is_in_use;
 };
 
-void				setup_fork(struct s_fork *fork);
+void		setup_fork(struct s_fork *fork);
+void		fork_be_grabbed(struct s_fork *fork);
+void		fork_be_dropped(struct s_fork *fork);
 
-int					create_forks(struct s_fork **forks, unsigned int n_forks);
-void				destroy_forks(struct s_fork **forks);
+int			create_forks(struct s_fork **forks, unsigned int n_forks);
+void		destroy_forks(struct s_fork **forks);
 
 #endif
--- phil_one/srcs/struct_fork.c
+++ phil_one/srcs/struct_fork.c
@@ -1,10 +1,23 @@
 #include "error.h"
 #include "struct_fork.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 void	setup_fork(struct s_fork *fork)
 {
-	fork->in_use = 0;
+	fork->is_in_use = false;
+}
+
+void	fork_be_grabbed(struct s_fork *fork)
+{
+	(void)fork;
+	////pthread_mutex_lock(fork);
+}
+
+void	fork_be_dropped(struct s_fork *fork)
+{
+	(void)fork;
+	//pthread_mutex_unlock(fork);
 }
 
 int		create_forks(struct s_fork **forks, unsigned int n_forks)
