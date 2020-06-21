c5400337b99c4ca1df6db5734c0c15fffbe26f75
mint <officialscientist42@gmail.com>
2020-06-21 12:27:12 -0700

    adding philosopher functionality

--- phil_one/includes/struct_phil.h
+++ phil_one/includes/struct_phil.h
@@ -1,35 +1,57 @@
 #ifndef STRUCT_PHIL_H
 # define STRUCT_PHIL_H
 
-enum					e_philosopher_options
-{
-	E_DEAD = 0,
-	E_EATING,
-	E_SLEEPING,
-	E_THINKING,
-};
-
 struct					s_state;
 
 /*
+**	id				philosopher id and id for forks
 **	tt_die			death countdown starting when you begin eating (ms)
 **	tt_eat			time takes to eat (ms)
 **	tt_sleep		time takes to sleep (ms)
 **	n_x_eaten		# meals eaten 
+**	is_dead			TRUE/FALSE
+**	is_full			TRUE/FALSE
+**	*forks[2]		two fork pointers. one for each hand
 */
 
-struct					s_phil
+
+enum				e_status
 {
-	unsigned int		id;
-	unsigned int		tt_die;
-	unsigned int		tt_eat;
-	unsigned int		tt_sleep;
-	unsigned int		n_x_eaten;
+	E_THINKING,
+	E_SLEEPING,
+	E_EATING,
+	E_FULL,
+	E_DEAD,
+	E_FORK,
 };
 
-void					setup_phil(struct s_phil *phil, unsigned int id, struct s_state *state);
+struct				s_phil
+{
+	unsigned int	id;
+	unsigned int	tt_die;
+	unsigned int	tt_eat;
+	unsigned int	tt_sleep;
+	unsigned int	n_x_eaten;
+	unsigned int	is_dead;
+	unsigned int	is_full;
+	struct s_fork	*forks[2];
+};
 
-int						create_phils(struct s_phil **phils, struct s_state *state);
-void					destroy_phils(struct s_phil **phil);
+void				setup_phil(struct s_phil *phil, unsigned int id,
+							struct s_state *state);
+
+void				phil_eat(struct s_phil *phil);
+void				phil_sleep(struct s_phil *phil);
+void				phil_think(struct s_phil *phil);
+void				phil_grab_fork(struct s_phil *phil, struct s_fork *fork,
+							struct s_state *state);
+void				phil_grab_forks(struct s_phil *phil, struct s_state *state);
+void				phil_drop_forks(struct s_phil *phil);
+void				phil_be_full(struct s_phil *phil);
+void				phil_die(struct s_phil *phil, struct s_state *state);
+void				phil_live(struct s_phil *phil);
+
+int					create_phils(struct s_phil **phils, struct s_state *state);
+void				destroy_phils(struct s_phil **phil);
 
 #endif
--- phil_one/srcs/struct_phil.c
+++ phil_one/srcs/struct_phil.c
@@ -1,7 +1,11 @@
 #include "error.h"
 #include "struct_state.h"
 #include "struct_phil.h"
+#include "struct_fork.h"
+#include "utils.h"
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
 
 void	setup_phil(struct s_phil *phil, unsigned int id, struct s_state *state)
 {
@@ -10,6 +14,104 @@ void	setup_phil(struct s_phil *phil, unsigned int id, struct s_state *state)
 	phil->tt_eat = state->tt_eat;
 	phil->tt_sleep = state->tt_sleep;
 	phil->n_x_eaten = 0;
+	phil->is_full = false;
+	phil->is_dead = false;
+	phil->forks[0] = &(state->forks[id - 1]);
+	phil->forks[1] = &(state->forks[(id < state->n_phils) ? id : 0]);
+}
+
+# define TIME 69420
+
+void	phil_print_status(struct s_phil *phil, enum e_status s)
+{
+	const char		*stats[6] = {
+		"is thinking",
+		"is sleeping",
+		"is eating",
+		"is full",
+		"is dead"
+		"has taken a fork",
+	};
+	char				buf[512];
+	unsigned int		len;
+	unsigned int		tmp_len;
+
+	len = 0;
+	util_uitoa(buf, TIME, 6, &tmp_len);
+	len = len + tmp_len;
+	buf[len] = ' ';
+	util_uitoa(buf + len, phil->id, 0, &tmp_len);
+	len = len + tmp_len;
+	buf[len] = ' ';
+	util_strcpy(buf + len, stats[s], &tmp_len);
+	len = len + tmp_len;
+	buf[len] = '\n';
+	write(STDOUT_FILENO, buf, len);
+}
+
+void	phil_eat(struct s_phil *phil)
+{
+	++(phil->n_x_eaten);
+	phil_print_status(phil, E_EATING);
+}
+
+void	phil_sleep(struct s_phil *phil)
+{
+	phil_print_status(phil, E_SLEEPING);
+}
+
+void	phil_think(struct s_phil *phil)
+{
+	phil_print_status(phil, E_THINKING);
+}
+
+void	phil_grab_fork(struct s_phil *phil, struct s_fork *fork, struct s_state *state)
+{
+	while (fork->is_in_use == true && phil->is_dead == false)
+		phil_live(phil);
+	if (phil->is_dead == true)
+	{
+		phil_die(phil, state);
+		return ;
+	}
+	fork_be_grabbed(fork);
+	phil_print_status(phil, E_FORK);
+}
+
+void	phil_grab_forks(struct s_phil *phil, struct s_state *state)
+{
+	phil_grab_fork(phil, phil->forks[0], state);
+	if (phil->is_dead == true)
+		return ;
+	phil_grab_fork(phil, phil->forks[1], state);
+	if (phil->is_dead == true)
+		return ;
+	return ;
+}
+
+void	phil_drop_forks(struct s_phil *phil)
+{
+	fork_be_dropped(phil->forks[0]);
+	fork_be_dropped(phil->forks[1]);
+//	phil->forks[0] = NULL;
+//	phil->forks[1] = NULL;
+}
+
+void	phil_be_full(struct s_phil *phil)
+{
+	phil->is_full = true;
+//	phil_print_status(phil, E_FULL);
+}
+
+void	phil_die(struct s_phil *phil, struct s_state *state)
+{
+	state->is_phil_dead = true;
+	phil_print_status(phil, E_DEAD);
+}
+
+void	phil_live(struct s_phil *phil)
+{
+	(void)phil;
 }
 
 int		create_phils(struct s_phil **phils, struct s_state *state)
